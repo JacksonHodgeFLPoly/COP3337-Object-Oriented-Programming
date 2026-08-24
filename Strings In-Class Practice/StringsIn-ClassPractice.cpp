d006a7737325a91f3c9e5e54d680eed5b741cb26
Jackson Hodge <jhodge2022@floridapoly.edu>
2026-08-24 14:25:42 -0400

    Initial commit

---
+++ Strings In-Class Practice/StringsIn-ClassPractice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main(void) {
+    std::string name, animal;
+
+    std::cout << "Enter your name: ";
+    getline(std::cin, name);
+    std::cout << "Enter your favorite animal: ";
+    getline(std::cin, animal);
+
+    std::string introduction = "Agent " + name;
+    introduction += ", also knows as The " + animal + ".";
+
+    std::cout << introduction << std::endl;
+    std::cout << "Your identity contains " << introduction.size() << "characters.\n";
+
+    return 0;
+}
