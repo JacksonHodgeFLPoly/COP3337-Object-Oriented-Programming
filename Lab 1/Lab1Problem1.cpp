ea56c1a7fb3076515b00ed072a0e0228987f2d1b
Jackson Hodge <jhodge2022@floridapoly.edu>
2026-08-25 17:26:31 -0400

    Fix

--- Lab 1/Lab1Problem1.cpp
+++ Lab 1/Lab1Problem1.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main(void) {
-    std::cout << "  CCCC    OOOO    PPP\n";
+    std::cout << "  CCCC    OOOO    PPP \n";
     std::cout << " C       O    O   P  P\n";
-    std::cout << "C       O      O  PPP\n";
-    std::cout << " C       O    O   P\n";
+    std::cout << "C       O      O  PPP \n";
+    std::cout << " C       O    O   P   \n";
     std::cout << "  CCCC    OOOO    P";
     return 0;
 }
